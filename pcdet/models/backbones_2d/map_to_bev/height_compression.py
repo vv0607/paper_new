1852595317855da62d010c5ac4ee668812330476
vv0607 <wufang0607@163.com>
2025-10-21 09:59:27 +0800

    Update height_compression.py

--- pcdet/models/backbones_2d/map_to_bev/height_compression.py
+++ pcdet/models/backbones_2d/map_to_bev/height_compression.py
@@ -14,13 +14,15 @@ class HeightCompression(nn.Module):
                 encoded_spconv_tensor: sparse tensor
         Returns:
             batch_dict:
-                spatial_features:
-
+                spatial_features: (N, C, H, W)
         """
         encoded_spconv_tensor = batch_dict['encoded_spconv_tensor']
         spatial_features = encoded_spconv_tensor.dense()
         N, C, D, H, W = spatial_features.shape
-        spatial_features = spatial_features.view(N, C * D, H, W)
+        
+        # 使用 max pooling 压缩 Z 轴，保持通道数为 C
+        spatial_features = spatial_features.max(dim=2)[0]  # (N, C, H, W)
+        
         batch_dict['spatial_features'] = spatial_features
         batch_dict['spatial_features_stride'] = batch_dict['encoded_spconv_tensor_stride']
         return batch_dict
