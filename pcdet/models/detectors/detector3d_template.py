46a3ee3f9bfadcb75f05edefdd084a864c0761be
vv0607 <wufang0607@163.com>
2025-10-21 10:00:26 +0800

    Update detector3d_template.py

--- pcdet/models/detectors/detector3d_template.py
+++ pcdet/models/detectors/detector3d_template.py
@@ -75,6 +75,8 @@ class Detector3DTemplate(nn.Module):
         )
         model_info_dict['module_list'].append(backbone_3d_module)
         model_info_dict['num_point_features'] = backbone_3d_module.num_point_features
+        if hasattr(backbone_3d_module, 'backbone_channels'):
+            model_info_dict['backbone_channels'] = backbone_3d_module.backbone_channels
         return backbone_3d_module, model_info_dict
 
     def build_map_to_bev_module(self, model_info_dict):
@@ -154,9 +156,11 @@ class Detector3DTemplate(nn.Module):
     def build_roi_head(self, model_info_dict):
         if self.model_cfg.get('ROI_HEAD', None) is None:
             return None, model_info_dict
+        roi_input_channels = model_info_dict.get('backbone_channels', model_info_dict['num_point_features'])
         point_head_module = roi_heads.__all__[self.model_cfg.ROI_HEAD.NAME](
             model_cfg=self.model_cfg.ROI_HEAD,
-            input_channels=model_info_dict['num_point_features'],
+            # input_channels=model_info_dict['num_point_features'],
+            input_channels=roi_input_channels,
             point_cloud_range=model_info_dict['point_cloud_range'],
             voxel_size=model_info_dict['voxel_size'],
             num_class=self.num_class if not self.model_cfg.ROI_HEAD.CLASS_AGNOSTIC else 1,
