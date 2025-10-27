d6ee5b31413c86dab1136007949a4dfbf0138f2b
vv0607 <wufang0607@163.com>
2025-10-27 21:36:24 +0800

    Update __init__.py

--- pcdet/models/backbones_2d/__init__.py
+++ pcdet/models/backbones_2d/__init__.py
@@ -1,7 +1,10 @@
 from .base_bev_backbone import BaseBEVBackbone
 from .easy_bev_backbone import EasyBEVBackbone
+from .cbam_attention import ImageBackboneWithCBAM, CBAM
 
 __all__ = {
     'BaseBEVBackbone': BaseBEVBackbone,
-    'EasyBEVBackbone': EasyBEVBackbone
+    'EasyBEVBackbone': EasyBEVBackbone,
+    'ImageBackboneWithCBAM': ImageBackboneWithCBAM,
+    'CBAM': CBAM,
 }
